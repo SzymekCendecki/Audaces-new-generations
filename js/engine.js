7a8210be373d92ea16131768e326265d8d68f73d
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-06-13 20:03:47 +0200

    added buttons to first menu and game title to header

--- js/engine.js
+++ js/engine.js
@@ -80,6 +80,25 @@ module.exports = __webpack_require__(1);
 document.addEventListener("DOMContentLoaded", function () {
   console.log("NIEWIERNE PSY RULEZ!!!!");
 
+  //funkcja, która tworzy nowy element DOM
+  function createNewElementAppend(nameElement, idName, text, whereAppend) {
+    var newElement = document.createElement(nameElement);
+    newElement.id = idName;
+    newElement.innerText = text;
+    whereAppend.append(newElement);
+  }
+
+  //utworzenie przycisków pierwszego menu
+  createNewElementAppend("p", "titleGameHeader", "AUDACES", $("header"));
+  createNewElementAppend("p", "subTitleGameHeader", "serce ze stali", $("#titleGameHeader"));
+  createNewElementAppend("button", "info", "info", $("nav"));
+  createNewElementAppend("button", "licence", "licencja", $("nav"));
+  createNewElementAppend("button", "tutorial", "tutorial", $("nav"));
+  createNewElementAppend("button", "newGame", "Nowa Gra", $("nav"));
+
+  //showanie przycisków pierszego menu
+  $("#info, #licence, #tutorial, #newGame, #titleGameHeader, #subTitleGameHeader").hide();
+
   //schowanie i usunięcie nazwy studia oraz sentencji
   $("#studioTitle").fadeOut(6000);
 
@@ -91,9 +110,21 @@ document.addEventListener("DOMContentLoaded", function () {
     $("#studioTitle").delay(2000).fadeOut(6000);
   }, 6200);
 
+  //zmiana koloru tła na beżowy
   setTimeout(function () {
     $("body").css("backgroundColor", "beige");
   }, 19000);
+
+  //usunięcie diva o id studioTitle
+  setTimeout(function () {
+    $("#studioTitle").remove();
+    $("#titleGameHeader, #subTitleGameHeader").fadeIn(1500);
+    $("#info, #licence, #tutorial, #newGame").fadeIn(1500).addClass("basicBtn");
+    $("#info").addClass("btnInfo");
+    $("#licence").addClass("btnLicence");
+    $("#tutorial").addClass("btnTutorial");
+    $("#newGame").addClass("btnNewGame");
+  }, 20000);
 });
 
 /***/ })
--- js/workingFile.js
+++ js/workingFile.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
 console.log("NIEWIERNE PSY RULEZ!!!!");
 
+//funkcja, która tworzy nowy element DOM
+function createNewElementAppend(nameElement, idName, text, whereAppend){
+		let newElement = document.createElement(nameElement);
+		newElement.id = idName;
+		newElement.innerText = text;
+		whereAppend.append(newElement);
+	}
+
+//utworzenie przycisków pierwszego menu
+  createNewElementAppend("p", "titleGameHeader", "AUDACES", $("header"));
+  createNewElementAppend("p", "subTitleGameHeader", "serce ze stali", $("#titleGameHeader"));
+  createNewElementAppend("button", "info", "info", $("nav"));
+  createNewElementAppend("button", "licence", "licencja", $("nav"));
+  createNewElementAppend("button", "tutorial", "tutorial", $("nav"));
+  createNewElementAppend("button", "newGame", "Nowa Gra", $("nav"));
+
+//showanie przycisków pierszego menu
+$("#info, #licence, #tutorial, #newGame, #titleGameHeader, #subTitleGameHeader").hide();
+
 //schowanie i usunięcie nazwy studia oraz sentencji
 $("#studioTitle").fadeOut(6000);
 
@@ -12,7 +31,22 @@ setTimeout(() =>{
   $("#studioTitle").delay(2000).fadeOut(6000);
 }, 6200);
 
+//zmiana koloru tła na beżowy
 setTimeout(() =>{
   $("body").css("backgroundColor", "beige");
 }, 19000);
+
+//usunięcie diva o id studioTitle
+setTimeout(()=>{
+  $("#studioTitle").remove();
+    $("#titleGameHeader, #subTitleGameHeader").fadeIn(1500);
+    $("#info, #licence, #tutorial, #newGame").fadeIn(1500).addClass("basicBtn");
+    $("#info").addClass("btnInfo");
+    $("#licence").addClass("btnLicence");
+    $("#tutorial").addClass("btnTutorial");
+    $("#newGame").addClass("btnNewGame");
+}, 20000);
+
+
+
 });
